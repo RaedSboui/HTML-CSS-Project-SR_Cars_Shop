463036e7c3b76528ae9a0450400fa7c3426d34c6
Raed <raedsboui1995@gmail.com>
2021-09-26 13:54:08 +0100

    full project

---
+++ Cars/FrenshCars/frenshCars.js
@@ -0,0 +1,21 @@
+/* START HUMBURGER */
+const humburgger = document.querySelector(".humburgger");
+const container = document.querySelector(".container");
+
+humburgger.addEventListener("click", () => {
+    container.classList.toggle("active");
+});
+/* END HUMBURGER */
+
+/* START TO TOP */
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll",()=>{
+    if(window.pageYOffset > 300){
+        toTop.classList.add("active");
+    }
+    else {
+        toTop.classList.remove("active");
+    }
+});
+/* START TO TOP */
